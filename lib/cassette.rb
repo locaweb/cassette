a1331f92fbb5c951b6ff2d86b0f24b46273a7d88
Pedro Lambert <pedrolambert@gmail.com>
2015-11-13 10:28:40 -0500

    Remove HTTP logic from base namespace

--- lib/cassette.rb
+++ lib/cassette.rb
@@ -43,29 +43,5 @@ module Cassette
     @@config = config
   end
 
-  def new_request(uri, timeout)
-    Faraday.new(url: uri, ssl: { verify: false, version: 'TLSv1' }) do |builder|
-      builder.adapter Faraday.default_adapter
-      builder.options.timeout = timeout
-    end
-  end
-
-  def post(uri, payload, timeout = DEFAULT_TIMEOUT)
-    perform(:post, uri, timeout) do |req|
-      req.body = URI.encode_www_form(payload)
-      logger.debug "Request: #{req.inspect}"
-    end
-  end
-
-  protected
-
-  def perform(op, uri, timeout = DEFAULT_TIMEOUT, &block)
-    request = new_request(uri, timeout)
-    res = request.send(op, &block)
-
-    res.tap do |response|
-      logger.debug "Got response: #{response.body.inspect} (#{response.status}), #{response.headers.inspect}"
-      Cassette::Errors.raise_by_code(response.status) unless response.success?
-    end
-  end
+  delegate :post, to: :'Http::Request'
 end
