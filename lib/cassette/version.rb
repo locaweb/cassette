60eb8f9dd5ac3b09348deddea650507e0f5c00a5
rogesson <rogesson.barboza@locaweb.com.br>
2020-03-19 20:34:38 -0300

    mend

--- lib/cassette/version.rb
+++ lib/cassette/version.rb
@@ -1,8 +1,8 @@
 module Cassette
   class Version
-    MAJOR = '1'
-    MINOR = '4'
-    PATCH = '0'
+    MAJOR = '1'.freeze
+    MINOR = '4'.freeze
+    PATCH = '0'.freeze
 
     def self.version
       [MAJOR, MINOR, PATCH].join('.')
