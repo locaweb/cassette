b2a1f97756fb7e579f20b6ec4c6faed38a854143
Claudio Martins <claudio.bruno.martins@gmail.com>
2020-11-25 11:45:24 -0300

    Fix when cas_extra_attributes keys is string

--- spec/cassette/authentication/user_factory_spec.rb
+++ spec/cassette/authentication/user_factory_spec.rb
@@ -38,5 +38,27 @@ RSpec.describe Cassette::Rubycas::UserFactory do
       it { is_expected.to be_customer }
       it { is_expected.not_to be_employee }
     end
+
+    context 'when key cas_extra_attributes is string' do
+      let(:session) do
+        name = Faker.name
+
+        {
+          cas_user: Faker::Internet.user_name(name),
+          cas_extra_attributes: {
+            'email' => Faker::Internet.email(name),
+            'type' => 'Customer',
+            'authorities' => '[CASTEST_ADMIN]'
+          }
+        }
+      end
+
+      its(:login) { is_expected.to eq(session[:cas_user]) }
+      its(:name) { is_expected.to eq(attributes['name']) }
+      its(:email) { is_expected.to eq(attributes['email']) }
+      its(:type) { is_expected.to eq(attributes['type'].downcase) }
+      it { is_expected.to be_customer }
+      it { is_expected.not_to be_employee }
+    end
   end
 end
