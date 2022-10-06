36033749839fab0dd184c2abb0086a83e309a843
Toshita Pandey <toshitapandey@gmail.com>
2022-10-06 15:08:13 +0530

    resolve rubocop rspec issues in cassette spec

--- spec/cassette_spec.rb
+++ spec/cassette_spec.rb
@@ -15,23 +15,19 @@ describe Cassette do
 
       described_class.config = config
 
-      expect(described_class.config)
-        .to eq(config)
+      expect(described_class.config).to eq(config)
     end
 
     context 'when tls_version was not specified' do
       it 'uses default TLS version: TLSv1_2' do
-        config = OpenStruct.new(
-          YAML.load_file('spec/config.yml')
-        )
+        config = OpenStruct.new(YAML.load_file('spec/config.yml'))
 
         # Removing tls_version field
         config.delete_field(:tls_version)
 
         described_class.config = config
 
-        expect(described_class.config.tls_version)
-          .to eq('TLSv1_2')
+        expect(described_class.config.tls_version).to eq('TLSv1_2')
       end
     end
 
@@ -43,24 +39,20 @@ describe Cassette do
 
         described_class.config = config
 
-        expect(described_class.config.tls_version)
-          .to eq('TLSv1_2')
+        expect(described_class.config.tls_version).to eq('TLSv1_2')
       end
     end
 
     context 'when verify_ssl was not specified' do
       it 'uses default verify_ssl value: false' do
-        config = OpenStruct.new(
-          YAML.load_file('spec/config.yml')
-        )
+        config = OpenStruct.new(YAML.load_file('spec/config.yml'))
 
         # Removing verify_ssl field
         config.delete_field(:verify_ssl)
 
         described_class.config = config
 
-        expect(described_class.config.verify_ssl)
-          .to be false
+        expect(described_class.config.verify_ssl).to be false
       end
     end
 
@@ -72,23 +64,25 @@ describe Cassette do
 
         described_class.config = config
 
-        expect(described_class.config.verify_ssl)
-          .to be true
+        expect(described_class.config.verify_ssl).to be true
       end
     end
   end
 
   describe '.logger' do
-    it 'returns a default instance' do
+    it 'returns a not nil' do
       expect(described_class.logger).not_to be_nil
+    end
+
+    it 'returns a default instance' do
       expect(described_class.logger.is_a?(Logger)).to be(true)
     end
 
     it 'returns rails logger when Rails is available' do
       keeping_logger do
         described_class.logger = nil
-        rails = double('Rails')
-        expect(rails).to receive(:logger).and_return(rails).at_least(:once)
+        rails = instance_double(described_class, 'Rails')
+        allow(rails).to receive(:logger).and_return(rails).at_least(:once)
         stub_const('Rails', rails)
         expect(described_class.logger).to eql(rails)
       end
